3de91abb6fa9fb101068d7365873eb0d880be93c
shurnster <slainuponmyshield@gmail.com>
2017-09-19 23:30:51 -0700

    refactored and renamed code

--- components/Page/Home.js
+++ components/Page/Home.js
@@ -5,60 +5,64 @@ import '../../styles/components/home.scss';
 class Home extends React.Component {
   render() {
     return (
-      <div>
-        <div className="container-fluid text-light banner-img">
-          <div>
-            <h1 className="display-3">Front-End Web Developer</h1>
-            <h1 className="lead">I specialize in Front-End development and love building new
-              and great things on projects with the latest technologies.
-            </h1>
-            <button className="btn btn-margin">
-              <Link to='/Projects'>See My Projects</Link>
-            </button>
+      
+      <div className="container-fluid">
+        <section>
+          <div className="row text-center text-light banner-img">
+            <div className="col-12">
+              <h1 className="display-3">Front-End Web Developer</h1>
+              <p className="lead">I specialize in Front-End development and love building new
+                and great things on projects with the latest technologies.
+              </p>
+              <button className="btn btn-portfolio m-5">
+                <Link to='/Projects'>See My Projects</Link>
+              </button>
+            </div>
           </div>
-        </div>
+        </section>
 
-        <div className="container-fluid p-5">
-          <div className="text-center header-name">
-            <h1 className="skills-header">My skills</h1>
+        <section>
+          <div className="row my-5">
+            <div className="col-12">
+              <h2 className="text-center skills-header">My Skills</h2>
+            </div>
           </div>
-        </div>
-        <div className="container-fluid container-margin text-center">
-          <div className="row">
+          <div className="row text-center py-5">
             <div className="col-md-4">
-              <i className="fa fa-file-text-o i-planning" aria-hidden="true"></i>
-              <h1 className="p-4 skills-header">Planning</h1>
+              <i className="fa fa-file-text-o mb-3 icon-fonts" aria-hidden="true"></i>
+              <h2 className="mb-3 skills-header-example">Planning</h2>
               <p className="text-left skills-p">I plan on the site’s structure and layout to
                 translate my thought process into a beautiful design specification for a great
                 user experience.</p>
             </div>
             <div className="col-md-4">
-              <i className="fa fa-pencil i-designing" aria-hidden="true"></i>
-              <h1 className="text-center p-4 skills-header">UX/UI Design</h1>
+              <i className="fa fa-pencil mb-3 icon-fonts" aria-hidden="true"></i>
+              <h2 className="mb-3 skills-header-example">UX/UI Design</h2>
               <p className="text-left skills-p">I sketch and wireframe interfaces and
                 translate it into a beautiful modern visual look on modern software</p>
             </div>
             <div className="col-md-4">
-              <i className="fa fa-code i-coding" aria-hidden="true"></i>
-              <h1 className="p-4 skills-header">Code</h1>
+              <i className="fa fa-code mb-3 icon-fonts" aria-hidden="true"></i>
+              <h2 className="mb-3 skills-header-example">Code</h2>
               <p className="text-left skills-p">Code Front-End coding combines the fundamental
                 principles of planning out the architecture and UX/UI design to craft a
                 beautiful modern website with the latest technologies.</p>
             </div>
           </div>
-        </div>
-
-        <div className="container-fluid text-light project-container">
-          <div className="text-center p-5">
-            <h2>My Recent Project</h2>
+        </section>
+        
+        <section>
+          <div className="row project-bg-color py-5">
+            <div className="col-12">
+              <h2 className="text-center text-light">My Recent Project</h2>
+            </div>
           </div>
-          <div className="row p-5">
+          <div className="row text-light project-bg-color py-5">
             <div className="col-md-6">
-              <div></div>
-              <h1>Background image goes here</h1>
+              <p>Background image goes here</p>
             </div>
             <div className="col-md-6">
-              <h1>Project Name</h1>
+              <h2>Project Name</h2>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
                 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
                 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
@@ -66,25 +70,27 @@ class Home extends React.Component {
                 dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
                 sunt in culpa qui officia deserunt mollit anim id est laborum</p>
               <b/>
-              <i className="fa fa-circle i-circle-yellow mr-1" aria-hidden="true"></i>
-              <span className="badge badge-dark mr-1">ReactJS</span>
-              <span className="badge badge-dark mr-1">ExpressJS</span>
-              <span className="badge badge-dark mr-1">Bootstrap 4</span>
-              <span className="badge badge-dark mr-1">React-Router</span>
+              <i className="fa fa-circle icon-fonts-circle mr-2" aria-hidden="true"></i>
+              <span className="badge badge-dark mr-2">ReactJS</span>
+              <span className="badge badge-dark mr-2">ExpressJS</span>
+              <span className="badge badge-dark mr-2">Bootstrap 4</span>
+              <span className="badge badge-dark mr-2">React-Router</span>
             </div>
           </div>
-        </div>
-        <div className="container-fluid text-center contact-container p-5">
-          <div className="row">
+        </section>
+        
+        <section>
+          <div className="row contact-container py-5">
             <div className="col-12">
-              <h1 className="m-3">Interested to work together?</h1>
-              <h2>I'm currently available. Don't hesitate to shoot me a contact!</h2>
+              <h2 className="m-3">Interested to work together?</h2>
+              <p className="lead">I'm currently available. Don't hesitate to shoot me a contact!</p>
               <button className="btn btn-portfolio m-5">
                 <Link to='/Contact'>Contact Me</Link>
               </button>
             </div>
           </div>
-        </div>
+        </section>
+
       </div>
     )
   }
