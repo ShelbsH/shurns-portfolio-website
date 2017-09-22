79eaad54a1d32e6c4fc3d4983ba2c620706b38be
shurnster <slainuponmyshield@gmail.com>
2017-09-22 07:53:23 -0700

    Added form validation functionality Added classnames dependency Added error styles classes in contact.scss

--- components/Form.js
+++ components/Form.js
@@ -1,51 +1,156 @@
-import React, {Component} from 'react';
+import React from 'react';
+import classNames from 'classnames';
 
-class Form extends Component {
+class Form extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      description: '',
+      isFirstNameError: false,
+      isLastNameError: false,
+      isEmailError: false,
+      isDescriptionError: false
+    }
+
+    this.onChangeHandler = this.onChangeHandler.bind(this);
+    this.onSubmitHandler = this.onSubmitHandler.bind(this);
+  }
+
+  onChangeHandler({target}) {
+    this.setState({
+      [target.name]: target.value
+    })
+  }
+
+  onSubmitHandler(e) {
+    e.preventDefault();
+
+    let isError = false;
+    const nameRegex = /^[a-z]+$/i;
+    const emailRegex = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+    const firstName = this.state.firstName;
+    const lastName = this.state.lastName;
+    const email = this.state.email;
+    const description = this.state.description;
+
+    const errors = {
+      isFirstNameError: false,
+      isLastNameError: false,
+      isEmailError: false,
+      isDescriptionError: false
+    }
+
+    if (!nameRegex.test(firstName.trim())) {
+      isError = true;
+      errors.isFirstNameError = true;
+    }
+
+    if (!nameRegex.test(lastName.trim())) {
+      isError = true;
+      errors.isLastNameError = true;
+    }
+
+    if (!emailRegex.test(email.trim())) {
+      isError = true;
+      errors.isEmailError = true;
+    }
+
+    if(!description.length) {
+      isError = true;
+      errors.isDescriptionError = true;
+    }
+
+    this.setState({
+      ...this.state,
+      ...errors
+    });
+
+    if (isError) {
+      return false;
+    }
+  }
+
   render() {
+
+    const labelClass = 'col-form-label form-label';
+    const inputClass = 'form-control form-control-lg input-text';
+    const textAreaClass = 'form-control form-control-lg textArea-size input-text';
+    const isFirstNameError = this.state.isFirstNameError;
+    const isLastNameError = this.state.isLastNameError;
+    const isEmailError = this.state.isEmailError;
+    const isDescriptionError = this.state.isDescriptionError;
+
+    const formClass = (defaultClass, errorClassName, isErrorState) => {
+      return classNames({
+        [defaultClass]: true,
+        [errorClassName]: isErrorState ? true : false
+      });
+    }
+
     return (
-      <form className="mt-5">
+      <form className="mt-5" onSubmit={this.onSubmitHandler}>
         <div className="form-row">
           <div className="form-group col-md-6">
-            <label className="col-form-label form-label" htmlFor="firstName">First Name*</label>
+            <label
+              className={formClass(labelClass, 'label-error', isFirstNameError)}
+              htmlFor="firstName">First Name*</label>
             <input
-              className="form-control form-control-lg input-text"
+              className={formClass(inputClass, 'input-error', isFirstNameError)}
               type="text"
               placeholder="Enter your first name"
-              id="firstName"/>
+              name="firstName"
+              value={this.state.firstName}
+              onChange={this.onChangeHandler}/>
           </div>
           <div className="form-group col-md-6">
-            <label className="col-form-label form-label" htmlFor="lastName">Last Name*</label>
+            <label
+              className={formClass(labelClass, 'label-error', isLastNameError)}
+              htmlFor="lastName">Last Name*</label>
             <input
-              className="form-control form-control-lg input-text"
+              className={formClass(inputClass, 'input-error', isLastNameError)}
               type="text"
               placeholder="Enter your last name"
-              id="lastName"/>
+              name="lastName"
+              value={this.state.lastName}
+              onChange={this.onChangeHandler}/>
           </div>
         </div>
 
         <div className="form-group">
-          <label className="col-form-label form-label" htmlFor="email">Email Address*</label>
+          <label 
+            className={formClass(labelClass, 'label-error', isEmailError)} 
+            htmlFor="email">Email Address*</label>
           <input
-            className="form-control form-control-lg input-text"
-            type="email"
-            id="email"
-            placeholder="Enter your email here"/>
+            className={formClass(inputClass, 'input-error', isEmailError)}
+            type="text"
+            placeholder="Enter your email here"
+            name="email"
+            value={this.state.email}
+            onChange={this.onChangeHandler}/>
         </div>
 
         <div className="form-group">
           <label htmlFor="organization" className="col-form-label form-label">Organization (optional)</label>
           <input
             className="form-control form-control-lg input-text"
-            id="organization"
+            name="organization"
             placeholder="Enter the name of your organization"/>
         </div>
 
         <div className="form-group">
-          <label className="col-form-label form-label" htmlFor="description">Enter a
-            description about your project or any other questions you have for me here*</label>
+          <label 
+            className={formClass(labelClass, 'label-error', isDescriptionError)}  
+            htmlFor="description">Enter a description about your project or any other questions you have for me here*
+          </label>
           <textarea
-            className="form-control form-control-lg textArea-size input-text"
-            id="description"></textarea>
+            className={formClass(textAreaClass, 'textArea-error', isDescriptionError)}
+            name="description"
+            value={this.state.description}
+            onChange={this.onChangeHandler}></textarea>
         </div>
 
         <button className="btn btn-lg form-btn-portfolio" type="submit">Submit</button>
