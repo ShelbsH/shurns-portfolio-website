cc1c310cdc99d3c2e440cfc99be4ac946f3e21d8
shurnster <slainuponmyshield@gmail.com>
2017-09-23 16:36:57 -0700

    added description content to the projects page

--- components/Page/Projects.js
+++ components/Page/Projects.js
@@ -2,8 +2,32 @@ import React from 'react';
 
 class Projects extends React.Component {
   render() {
+
+    const renderProjects = ( props ) => {
+      return props.map((item, idx) => {
+        return (
+          <div className="row mt-5" key={idx}>
+            <div className="col-md-6">
+              <h2>Background images goes here...</h2>
+            </div>
+            <div className="col-md-6">
+              <h2 className="mt-4">{item.website}</h2>
+              <p>{item.description}</p>
+            </div>
+          </div>
+        )
+      }); 
+    }
+    
     return (
-      <h1>Projects Page</h1>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-12">
+            <h1 className="display-4 text-center mt-5">My Projects</h1>
+          </div>
+        </div>
+        {renderProjects(this.props.data)}
+      </div>
     )
   }
 }
