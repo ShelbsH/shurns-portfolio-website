4d0b03f516aec36572f6b2709de03a71779e452b
shurnster <slainuponmyshield@gmail.com>
2017-09-30 20:27:55 -0700

    Added express Routers Added nodemailer Added nodemailer-mailgun-transport Used an email API for sending emails

---
+++ api/email.js
@@ -0,0 +1,41 @@
+const router = require('express').Router();
+const nodemailer = require('nodemailer');
+const mailgunTransport = require('nodemailer-mailgun-transport');
+const bodyParser = require('body-parser');
+
+router.use(bodyParser.urlencoded({extended: false}));
+router.use(bodyParser.json());
+
+router.post('/send', (req, res) => {
+  const { firstName, lastName, email, organization, description } = req.body;
+
+  const transporter = nodemailer.createTransport(mailgunTransport({
+    auth: {
+      domain: 'sandbox46c4ae8bf6944c2786212d738f88cef9.mailgun.org',
+      api_key: 'key-11de6f728b88e6637a31324a6313a038'
+    }
+  }));
+
+  const descriptionHTML = `
+  <h1>Shurns Portfolio Support Question</h1>
+  <h2>Name: ${firstName + ' ' + lastName}</h2>
+  <h2>Email Address: ${email}</h2>
+  <h2>Organization: ${organization || 'Not listed'}</h2>
+  <p>${description}</p>`;
+
+  const mailOptions = {
+    from: 'Shurns Portfolio Support <slainuponmyshield@gmail.com>',
+    to: 'slainuponmyshield@gmail.com',
+    subject: 'Shurns Portfolio Support Question',
+    html: descriptionHTML
+  };
+
+  transporter.sendMail(mailOptions, (err, info) => {
+    if(err) {
+      console.log(err);
+    }
+    res.status(200).send('Email has successfully sent!');
+  });
+});
+
+module.exports = router;
--- index-server.js
+++ index-server.js
@@ -1,12 +1,16 @@
-
 //=============================================
 // Express
 const express = require('express');
 const app = express();
 
 //=============================================
-// Regular dependency tools for express
+// Get Router middleware path
+const emailRouter = require('./api/email');
+
+//=============================================
+// Dependency tools for express
 const path = require('path');
+const morgan = require('morgan');
 
 //=============================================
 // Port number for when the application is running
@@ -33,6 +37,8 @@ app.use(webpackDevMiddleware(compiler, {
 
 app.use(require("webpack-hot-middleware")(compiler));
 
+morgan('dev');
+
 //Sets the file path directory to load up scripts and styles from the HTML file.
 app.use(express.static(filePath));
 
@@ -43,6 +49,9 @@ app.use(express.static(imagePath));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views/index.html'));
 });
+
+//Load up the Route middleware upon root index GET request
+app.use('/', emailRouter);
 
 //Send image files based on GET request.
 app.get(/^\/.*\.(png|jpg)$/, (req, res) => {
